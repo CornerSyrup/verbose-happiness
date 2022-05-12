8bad0b5db8f86e84b0a6f4ca406b22a7557ca0ea
ths95049 <hal.school.kleinc@outlook.com>
2022-05-12 05:14:47 +0000

    Initial Commit

---
+++ app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.hal.tokyo.kadai01_is14a_95049
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

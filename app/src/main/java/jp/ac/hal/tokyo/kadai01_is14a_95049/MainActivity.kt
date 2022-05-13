6ba72c0e2f33d71b546f67b16904342025972bbe
ths95049 <hal.school.kleinc@outlook.com>
2022-05-13 07:08:12 +0000

    Add fragment nav routing
    - re-layout main avtivity
    - add dependency for nav resource

--- app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/MainActivity.kt
+++ app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/MainActivity.kt
@@ -1,11 +1,18 @@
 package jp.ac.hal.tokyo.kadai01_is14a_95049
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<BottomNavigationView>(R.id.main_nav_view).setupWithNavController(
+            (supportFragmentManager.findFragmentById(R.id.main_nav_container) as NavHostFragment).navController
+        )
     }
 }

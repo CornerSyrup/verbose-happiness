c0181fd73c056effe039b285a39cff5186628864
ths95049 <hal.school.kleinc@outlook.com>
2022-05-13 15:54:18 +0000

    Add string resources for enum
    i18n

---
+++ app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/ItemCategory.kt
@@ -0,0 +1,35 @@
+package jp.ac.hal.tokyo.kadai01_is14a_95049
+
+import androidx.annotation.StringRes
+
+enum class ItemCategory(@StringRes val res: Int) {
+    // その他
+    Others(R.string.item_category_others),
+
+    // 飲食
+    Food(R.string.item_category_food),
+
+    // 水道光熱
+    PublicBusiness(R.string.item_category_public),
+
+    // 住宅
+    RealEstate(R.string.item_category_estate),
+
+    // 通信
+    Telecom(R.string.item_category_telecom),
+
+    // 車両
+    Car(R.string.item_category_car),
+
+    // 学費
+    School(R.string.item_category_school),
+
+    // 税金
+    Tax(R.string.item_category_tax),
+
+    // 交通
+    Transportation(R.string.item_category_transport),
+
+    // 購読・定期
+    Subscriptions(R.string.item_category_subscribe),
+}

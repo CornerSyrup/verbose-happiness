6fe6b3857326378e127a2be6a486451003c40081
ths95049 <hal.school.kleinc@outlook.com>
2022-05-20 09:55:58 +0000

    Remove debug code

--- app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/DetailPageViewModel.kt
+++ app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/DetailPageViewModel.kt
@@ -1,7 +1,5 @@
 package jp.ac.hal.tokyo.kadai01_is14a_95049
 
-import android.util.Log
-import android.view.View
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import jp.ac.hal.tokyo.kadai01_is14a_95049.data.RegisterItem
@@ -13,10 +11,6 @@ class DetailPageViewModel : BaseObservable() {
 
     init {
         refresh()
-    }
-
-    fun debugClick(view: View) {
-        Log.i("DB", "debugClick: $items")
     }
 
     val adapter: RegisterItemsAdapter

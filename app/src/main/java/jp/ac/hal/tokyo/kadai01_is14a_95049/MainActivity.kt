c0e19933f66495c2826945b6f1ba35d964c52330
ths95049 <hal.school.kleinc@outlook.com>
2022-05-20 05:13:27 +0000

    Initialize Android Three Ten
    It's required to get system time locale

--- app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/MainActivity.kt
+++ app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jakewharton.threetenabp.AndroidThreeTen
 import jp.ac.hal.tokyo.kadai01_is14a_95049.data.RegisteredItemDb
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,6 @@ class MainActivity : AppCompatActivity() {
             (supportFragmentManager.findFragmentById(R.id.main_nav_container) as NavHostFragment).navController
         )
         RegisteredItemDb.setUpRegisterItemDb(this)
+        AndroidThreeTen.init(this)
     }
 }

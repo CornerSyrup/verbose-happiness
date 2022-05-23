7863ac18e213c9612ef4eec421e9002c1f475e80
ths95049 <hal.school.kleinc@outlook.com>
2022-05-23 01:32:35 +0000

    Add mode feature to register page

--- app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/ui/RegisterPageViewModel.kt
+++ app/src/main/java/jp/ac/hal/tokyo/kadai01_is14a_95049/ui/RegisterPageViewModel.kt
@@ -7,13 +7,15 @@ import androidx.databinding.Bindable
 import androidx.navigation.findNavController
 import com.microsoft.fluentui.datetimepicker.DateTimePickerDialog
 import jp.ac.hal.tokyo.kadai01_is14a_95049.BR
+import jp.ac.hal.tokyo.kadai01_is14a_95049.R
 import jp.ac.hal.tokyo.kadai01_is14a_95049.data.RegisterItem
 import jp.ac.hal.tokyo.kadai01_is14a_95049.data.RegisteredItemDb
 import org.threeten.bp.Duration
 import org.threeten.bp.ZoneId
 import org.threeten.bp.ZonedDateTime
 
-class RegisterPageViewModel(val model: RegisterItem) : BaseObservable() {
+class RegisterPageViewModel(val model: RegisterItem, private val mode: RegisterPageMode) :
+    BaseObservable() {
     init {
         this.model.apply {
             ZonedDateTime.now().let {
@@ -32,6 +34,22 @@ class RegisterPageViewModel(val model: RegisterItem) : BaseObservable() {
     val date: String
         get() = "${model.year} / ${model.month} / ${model.day}"
 
+    @get:Bindable
+    val commitText: Int
+        get() = when (mode) {
+            RegisterPageMode.Create -> R.string.register_btn_reg
+            RegisterPageMode.Copy -> R.string.register_btn_dup
+            RegisterPageMode.Edit -> R.string.register_btn_edt
+        }
+
+    @get:Bindable
+    val commitCommand: View.OnClickListener
+        get() = when (mode) {
+            RegisterPageMode.Create -> View.OnClickListener { view -> register(view) }
+            RegisterPageMode.Copy -> View.OnClickListener { view -> clone(view) }
+            RegisterPageMode.Edit -> View.OnClickListener { view -> update(view) }
+        }
+
     private val modelZonedDate: ZonedDateTime
         get() = ZonedDateTime.of(
             model.year, model.month, model.day,
@@ -42,6 +60,17 @@ class RegisterPageViewModel(val model: RegisterItem) : BaseObservable() {
     fun register(view: View) {
         RegisteredItemDb.getRegisterItemDb()?.registerItemDao()?.insert(model)
         view.findNavController().navigate(RegisterPageDirections.actionRegisterPageToDetailPage())
+    }
+
+    fun update(view: View) {
+        RegisteredItemDb.getRegisterItemDb()?.registerItemDao()?.update(model)
+        view.findNavController().navigate(RegisterPageDirections.actionRegisterPageToDetailPage())
+    }
+
+    fun clone(view: View) {
+        RegisteredItemDb.getRegisterItemDb()?.registerItemDao()?.insert(model.copy(id = 0))
+        view.findNavController().navigate(RegisterPageDirections.actionRegisterPageToDetailPage())
+
     }
 
     fun cancel(view: View) {
